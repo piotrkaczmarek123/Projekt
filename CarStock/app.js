697fc159530832edfacd09a53fcc99e75fdb30ae
piotr kaczmarek <89982700+piotrkaczmarek123@users.noreply.github.com>
2023-05-08 11:37:13 +0200

    wersja 2

--- CarStock/app.js
+++ CarStock/app.js
@@ -4,17 +4,23 @@ const app = express();
 const fs = require('fs');
 
 
-app.get('/allStock', (req,res) => {
-    res.send("siema");   
+app.get('/allStock/:id', (req, res) => {
     const carsData = fs.readFileSync('./data.json');
-    // do tablicy
     const cars = JSON.parse(carsData);
-    
-    console.log(cars);
+
+    const car = cars.find(car => car.id == req.params.id); //   pozwala na pobieranie wartości z parametrów ścieżki URL (id) przez funkcje parms
+
+    if (!car) {
+        res.send('Samochód nie został znaleziony'); // zwraca błąd gdy samochód nie został znaleziony
+    } else {
+        res.send(`Model: ${car.model}, rok produkcji: ${car.modelYear}, moc: ${car.power}, kolor: ${car.color}`); // wyświetla informacje o samochodzie
+    }
 });
 
 app.listen(3000, () => {
     console.log("serwer dziala na procie 3000");
 });
+
+
 
 
